8bed299fa9745071deba960a24cacdc84539669a
Anmol Nagpal <ianmolnagpal@gmail.com>
2019-07-07 16:52:37 +0200

    update

--- _test/efs_test.go
+++ _test/efs_test.go
@@ -7,7 +7,7 @@ package test
 import (
 	"testing"
 	//"strings"
-	//"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/assert"
 	"github.com/gruntwork-io/terratest/modules/terraform"
 )
 
@@ -15,10 +15,9 @@ func TestEFS(t *testing.T) {
 	t.Parallel()
 
 	terraformOptions := &terraform.Options{
-
 		// Source path of Terraform directory.
 		TerraformDir: "../_example/complete",
-		Upgrade: true,
+		Upgrade:      true,
 	}
 
 	// This will run `terraform init` and `terraform apply` and fail the test if there are any errors
@@ -26,4 +25,10 @@ func TestEFS(t *testing.T) {
 
 	// At the end of the test, run `terraform destroy` to clean up any resources that were created
 	defer terraform.Destroy(t, terraformOptions)
+
+	// Run `terraform output` to get the value of an output variable
+	keyArn := terraform.Output(t, terraformOptions, "efs_arn")
+
+	// Verify we're getting back the outputs we expect
+	assert.Contains(t, keyArn, "arn:aws:elasticfilesystem:us-east-1:866067750630:file-system/")
 }
