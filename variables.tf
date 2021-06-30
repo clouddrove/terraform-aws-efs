10bb703f4d7e5df301b02b1b5e27c53ed828d2c1
yadavprakash <yadavprakash.it@gmail.com>
2021-06-30 19:57:23 +0530

    update github-action,module tags and update in 0.15

--- variables.tf
+++ variables.tf
@@ -6,7 +6,7 @@ variable "name" {
 
 variable "repository" {
   type        = string
-  default     = "https://registry.terraform.io/modules/clouddrove/subnet/aws/0.14.0"
+  default     = "https://github.com/clouddrove/terraform-aws-efs"
   description = "Terraform current module repo"
 
   validation {
